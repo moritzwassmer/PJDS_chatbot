dbb1cf7af0e88ff2006726637dd1c4ba7164876d
sechs130 <s.echsle@web.de>
2022-06-13 14:33:52 +0200

    test better import in GUI

--- CodingDirectory/Clustering/Modules/question_generator.py
+++ CodingDirectory/Clustering/Modules/question_generator.py
@@ -1,3 +1,7 @@
 class QuestionGenerator():
+
     def __init__(self) -> None:
         pass
+    
+    def generate_question(self, words) -> str:
+        return "Geht es bei Ihrem Anliegen um {0} ?".format(" ".join(words))
